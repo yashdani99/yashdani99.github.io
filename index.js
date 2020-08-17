d208aca51773d4582bed537e8d787c95ca55c19f
Yash Dani <yashdani99@yahoo.com>
2020-08-17 12:32:04 -0700

    Updated site's content and some design

--- index.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  setTimeout(function(){ $("#profile").css("display", "block") }, 0);
-  $("#profile").fadeIn(3500);
-  $("#quote").fadeIn(4500);
-  $("#name").fadeIn(3500);
-});
